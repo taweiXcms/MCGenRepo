9258c0293884d1795608134510a5307fd24fec0a
Ta-Wei <ta-wei.wang@cern.ch>
2015-12-18 14:50:07 +0100

    Add DY/Z private prod.

---
+++ DYToXX_M_40_TuneCUETP8M1_5TeV_pythia8_20151209/GenFrag/fragment/python/DYToMuMu_M_40_TuneCUETP8M1_5TeV_pythia8.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import * 
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import * 
+
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    crossSection = cms.untracked.double(2423.9),
+    comEnergy = cms.double(5020.0),
+
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock, 
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring('WeakSingleBoson:ffbar2gmZ = on',
+                                        'WeakZ0:gmZmode = 0',
+                                        'PhaseSpace:mHatMin = 40.',
+                                        '23:onMode = off',
+                                        '23:onIfAny = 13', ),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythia8CommonSettings',
+            'pythia8CUEP8M1Settings', 
+            'processParameters')
+    )
+)
